bd2225c81f91e580c1960d48641e45fd55a2c50c
fatima-zahra-github-account <fatimakandhro@gmail.com>
2024-06-26 16:37:40 -0700

    'q7.commit'

---
+++ q.7Number/main.ts
@@ -0,0 +1,25 @@
+//Number Eight: Write addition, subtraction, multiplication, and division operations that each
+//result in the number 8. Be sure to enclose your operations in print statements to see the
+//results.
+
+//addition
+
+console.log(3+5);
+
+//subtraction
+
+console.log(20-12);
+
+//multiplication
+
+console.log(2 * 4);
+
+//division
+
+console.log(16 / 2);
+
+
+
+
+
+ 
