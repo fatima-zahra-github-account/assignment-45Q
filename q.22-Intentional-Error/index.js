68f7569a2f568b58c09063d4e3690d3de489d91f
fatima-zahra-github-account <fatimakandhro@gmail.com>
2024-07-04 16:08:02 -0700

    q.commit

---
+++ q.22-Intentional-Error/index.js
@@ -0,0 +1,9 @@
+// Intentional Error: If you haven’t received an array index error in one of your programs
+//  yet, try to make one happen. Change an index in one of your programs to produce an index
+//   error. Make sure you correct the error before closing the program
+let nameArray = ["fatima", "zahra", "ali"];
+let name2 = nameArray[5];
+let name3 = nameArray[1];
+console.log(name2);
+console.log(name3);
+export {};
---
+++ q.23-Conditional-Tests/index.ts
@@ -0,0 +1,59 @@
+// Conditional Tests: Write a series of conditional tests. Print a statement describing 
+// each test and your prediction for the results of each test. Your code should look 
+// something like this:
+let car = 'subaru';
+//1
+
+console.log("Is car == 'subaru'? I predict True.")
+console.log(car == 'subaru')
+//2
+
+console.log("Is car == 'toyota'? I predict false.")
+console.log(car == 'toyota')
+//3
+
+console.log("Is car == 'toyota'? I predict true.")
+console.log(car !== 'toyota')
+//4
+
+console.log("Is car == 'toyota'? I predict false.")
+console.log(car === 'toyota')
+//5
+
+console.log("Is car === 'honda'? I predict false.")
+console.log(car === "honda")
+//6
+
+console.log("Is car !== 'boat'? I predict true.")
+console.log(car !== 'boat')
+//7
+
+console.log("Is car !== 'bus'? I predict true.")
+console.log(car !== 'bus')
+//8
+console.log("Is car === 'suzuki'? I predict false.")
+console.log(car === 'suzuki')
+//9
+console.log("Is car !== 'cycle'? I predict true.")
+console.log(car !== 'cycle')
+//10
+console.log("Is car !== 'rikshaw' ? I predict true.")
+console.log(car !== 'bus')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
