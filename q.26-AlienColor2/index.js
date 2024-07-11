ceb673f3f9b4313f9aca01ed5ac601cecdc7bc8a
fatima-zahra-github-account <fatimakandhro@gmail.com>
2024-07-11 15:28:58 -0700

    q.commit

---
+++ q.26-AlienColor2/index.js
@@ -0,0 +1,17 @@
+let alienColor = "green";
+//====================== version 1 =================
+if (alienColor === "green") {
+    console.log("player just earned 5 points.");
+}
+else {
+    console.log("the player just earned 10 points");
+}
+// ------------------------------------------------------------
+// ================ version 2 ====================
+if (alienColor === "yellow") {
+    console.log("the player just earned 10 points");
+}
+else {
+    console.log("the player just earned 10 points");
+}
+export {};
