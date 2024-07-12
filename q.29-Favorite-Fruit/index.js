b2fd904bb5abfdaec7b1ade0cb7e5f28671e5225
fatima-zahra-github-account <fatimakandhro@gmail.com>
2024-07-11 17:27:25 -0700

    q.29-commit

---
+++ q.29-Favorite-Fruit/index.js
@@ -0,0 +1,17 @@
+"use strict";
+let fruitName = ["mango", "banana", "apple"];
+if (fruitName.includes("orange")) {
+    console.log("i really like orange");
+}
+if (fruitName.includes("mango")) {
+    console.log("i really like mango");
+}
+if (fruitName.includes("banana")) {
+    console.log("i really like banana");
+}
+if (fruitName.includes("apple")) {
+    console.log("i really like apple");
+}
+if (fruitName.includes("lechi")) {
+    console.log("i really like lechi");
+}
---
+++ q.29-Favorite-Fruit/index.ts
@@ -0,0 +1,21 @@
+let fruitName : string[] = ["mango","banana","apple"]
+
+if(fruitName.includes("orange")){
+    console.log("i really like orange");    
+}
+if(fruitName.includes("mango")){
+    console.log("i really like mango");
+    
+}
+if(fruitName.includes("banana")){
+    console.log("i really like banana");
+    
+}
+if(fruitName.includes("apple")){
+    console.log("i really like apple");
+    
+}
+if(fruitName.includes("lechi")){
+    console.log("i really like lechi");
+    
+}
