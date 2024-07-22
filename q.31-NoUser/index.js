2ca3f5fd0902bbfc44e194adb769404dbf080e87
fatima-zahra-github-account <fatimakandhro@gmail.com>
2024-07-22 11:35:20 -0700

    Q31

---
+++ q.31-NoUser/index.js
@@ -0,0 +1,8 @@
+"use strict";
+let userName = ["fatima", "zahra", "admin", "Ali", "Hasnain"];
+while (userName.length > 0) {
+    userName.pop();
+}
+if (userName.length === 0) {
+    console.log("we need to find some user!");
+}
---
+++ q.31-NoUser/index.ts
@@ -0,0 +1,9 @@
+let userName: string[] = ["fatima","zahra","admin","Ali","Hasnain"]
+
+while(userName.length > 0){
+    userName.pop()
+}
+if(userName.length === 0){
+    console.log("we need to find some user!");
+    
+}
