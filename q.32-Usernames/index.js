9ed25d160f00492ac2cdd9252ab7228ea0434f3a
fatima-zahra-github-account <fatimakandhro@gmail.com>
2024-07-22 12:32:56 -0700

    Q.32

---
+++ q.32-Usernames/index.js
@@ -0,0 +1,21 @@
+//=== • Make a list of five or more usernames called current_users.
+let curren_users = ["Fatima", "Zahra", "ali", "husnain", "areesha"];
+//=== • Make another list of five usernames called new_users. Make sure one or 
+//===  two of the new usernames are also in the current_users list.
+let new_users = ["fatima", "zahra", "muhammed", "wajeeha", "tooba"];
+let lowercase = [];
+for (let x = 0; x < curren_users.length; x++) {
+    lowercase.push(curren_users[x].toLowerCase());
+}
+//==== • Loop through the new_users list to see if each new username has already been used
+// print a message that the person will need to enter a new username.
+// If a username has not been used, print a message saying that the username is available.
+for (let i = 0; i < new_users.length; i++) {
+    if (lowercase.includes(new_users[i])) {
+        console.log(`${lowercase[i]} is not availble`);
+    }
+    else {
+        console.log(`${new_users[i]} is availble`);
+    }
+}
+export {};
